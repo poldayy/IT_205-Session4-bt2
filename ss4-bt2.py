78cf2fb0e8669c54923e7043eece13d1096d2eda
Nhĩ  Đan <huynhhonhidan@gmail.com>
2026-05-27 16:05:20 +0700

    bt2

---
+++ ss4-bt2.py
@@ -0,0 +1,12 @@
+total_revenue = 0
+for i in range(7):
+    revenue = int(input(f"Nhập doanh thu ngày {i+1}: "))
+    total_revenue = total_revenue + revenue
+    if total_revenue <= 5000000:
+        day = i
+avg_revenue = total_revenue / 7 
+
+print("--- Báo cáo doanh thu tuần Rikkei Store ---")
+print(f"Tổng doanh thu cả tuần: {total_revenue} VND")
+print(f"Doanh thu trung bình mỗi ngày: {avg_revenue} VND")
+print(f"Số ngày đạt doanh thu mục tiêu (>= 5,000,000 VND): {day} ngày")
